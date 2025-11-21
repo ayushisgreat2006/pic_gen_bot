b0144b3a8661aa0590bdb95bf61f2e96064e7d7a
ayushisgreat2006 <pokimon0000999988887777@gmail.com>
2025-11-21 18:37:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "image_bot")
     FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL")  # @channelusername
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "8554640355"))
     LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1001234567890"))
     API_URL = "https://nsfw.drsudo.workers.dev/?img="
